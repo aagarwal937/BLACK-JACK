42a80ae8c69cf7186fce8a6bfa7cb7010fbe7ab8
Abhishek Agarwal <55159395+aagarwal937@users.noreply.github.com>
2019-10-25 17:17:24 +0530

    Add files via upload

---
+++ blackjack8.py
@@ -0,0 +1,17 @@
+def hit_or_stand(deck,hand):
+    global playing  # to control an upcoming while loop
+    
+    while True:
+        x = input("Would you like to Hit or Stand? Enter 'h' or 's' ")
+        
+        if x[0].lower() == 'h':
+            hit(deck,hand)
+
+        elif x[0].lower() == 's':
+            print("Player stands. Dealer is playing.")
+            playing = False
+
+        else:
+            print("Sorry, please try again.")
+            continue
+        break
---
+++ blackjack9.py
@@ -0,0 +1,12 @@
+def take_bet(chips):
+    
+    while True:
+        try:
+            chips.bet = int(input('How many chips would you like to bet? '))
+        except ValueError:
+            print('Sorry, a bet must be an integer!')
+        else:
+            if chips.bet > chips.total:
+                print("Sorry, your bet can't exceed",chips.total)
+            else:
+                break
